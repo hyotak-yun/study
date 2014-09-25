bebfba56cfb7a868a9dc66507ed7979ddc88f2d8
devtak <devtak@naver.com>
2014-09-25 17:21:39 +0900

    init project.

---
+++ Classes/GameScene.cpp
@@ -0,0 +1,9 @@
+//
+//  GameScene.cpp
+//  testgame
+//
+//  Created by devtak on 2014. 9. 25..
+//
+//
+
+#include "GameScene.h"
---
+++ Classes/GameScene.h
@@ -0,0 +1,14 @@
+//
+//  GameScene.h
+//  testgame
+//
+//  Created by devtak on 2014. 9. 25..
+//
+//
+
+#ifndef __testgame__GameScene__
+#define __testgame__GameScene__
+
+#include <stdio.h>
+
+#endif /* defined(__testgame__GameScene__) */
